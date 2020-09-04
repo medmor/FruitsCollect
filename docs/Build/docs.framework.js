23bc8630ba51305ca18c78518cd1baf653b4cca0
medmor <medmor@users.noreply.github.com>
2020-09-04 15:58:14 +0100

    asset2

--- docs/Build/docs.framework.js
+++ docs/Build/docs.framework.js
@@ -1299,7 +1299,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2846576;
+STATICTOP = STATIC_BASE + 2846912;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3229,7 +3229,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2846576;
+var STATIC_BUMP = 2846912;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
